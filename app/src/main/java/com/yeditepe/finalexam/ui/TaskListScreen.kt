3e7a8e7dc0fadcd4fd20650aa676251dda912ce4
Efe Ceylan <efeceylan0099@gmail.com>
2026-01-06 15:43:05 +0300

    Update TaskListScreen.kt

--- app/src/main/java/com/yeditepe/finalexam/ui/TaskListScreen.kt
+++ app/src/main/java/com/yeditepe/finalexam/ui/TaskListScreen.kt
@@ -14,10 +14,14 @@ import com.yeditepe.finalexam.viewmodel.TaskViewModel
 fun TaskListScreen(viewModel: TaskViewModel = viewModel()) {
 
     // TODO 3: Read task list from ViewModel
+    val tasks = viewModel.tasks
 
     Column {
         // TODO 4: Display task titles and completion status
         // Use a simple Column or LazyColumn
+        tasks.forEach { task ->
+            Text(text = "${task.title} - ${if (task.isCompleted) "Completed" else "Not Completed"}")
+        }
     }
 }
 
@@ -25,6 +29,7 @@ fun TaskListScreen(viewModel: TaskViewModel = viewModel()) {
 fun TaskRow(task: Task, navController: NavController) {
 
     Text(
+
         text = task.title,
         modifier = Modifier.clickable {
             // TODO 3: Navigate to detail screen with task title
